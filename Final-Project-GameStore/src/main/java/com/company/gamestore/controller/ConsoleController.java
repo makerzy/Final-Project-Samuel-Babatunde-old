01b5aed9ab123e45e9f5065b32082fd099e80f20
alpnelson <alpnelson5@gmail.com>
2023-08-29 20:49:35 -0400

    adding html

--- Final-Project-GameStore/src/main/java/com/company/gamestore/controller/ConsoleController.java
+++ Final-Project-GameStore/src/main/java/com/company/gamestore/controller/ConsoleController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.List;
 import java.util.Optional;
 
@@ -14,7 +15,6 @@ import java.util.Optional;
 public class ConsoleController {
     @Autowired
     ConsoleRepository ConRepo;
-
 
     // read all console
     @GetMapping("/console")
