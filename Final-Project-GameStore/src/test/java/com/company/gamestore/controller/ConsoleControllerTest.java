d1f98dff8ef626991fbd428951d228b0a88f6ca6
alpnelson <alpnelson5@gmail.com>
2023-08-30 13:52:29 -0400

    Console Controller test

--- Final-Project-GameStore/src/test/java/com/company/gamestore/controller/ConsoleControllerTest.java
+++ Final-Project-GameStore/src/test/java/com/company/gamestore/controller/ConsoleControllerTest.java
@@ -1,17 +1,30 @@
 package com.company.gamestore.controller;
 
 import com.company.gamestore.model.Console;
+import com.company.gamestore.model.Game;
 import com.company.gamestore.model.TShirt;
 import com.company.gamestore.repository.ConsoleRepository;
 import com.company.gamestore.repository.TShirtRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @AutoConfigureMockMvc(addFilters = false)
 @WebMvcTest(ConsoleController.class)
 class ConsoleControllerTest {
@@ -22,6 +35,108 @@ class ConsoleControllerTest {
     @MockBean
     private ConsoleRepository consoleRepository;
     private Console console;
+
+    @BeforeEach
+    public void setUp() {
+        console = new Console();
+        console.setModel("PlayStation 5");
+        console.setManufacturer("Sony");
+        console.setMemoryAmount("825GB SSD");
+        console.setProcessor("Custom AMD Zen 2");
+        console.setPrice(499.99);
+        console.setQuantity(100);
+
+    }
+
+    @Test
+    public void shouldCreateNewGame() throws Exception {
+        when(consoleRepository.save(any(Console.class))).thenReturn(console);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/consoles")
+                        .content(mapper.writeValueAsString(console))
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.model").value("PlayStation 5"))
+                .andExpect(jsonPath("$.manufacturer").value("Sony"))
+                .andExpect(jsonPath("$.memoryAmount").value("825GB SSD"))
+                .andExpect(jsonPath("$.processor").value("Custom AMD Zen 2"))
+                .andExpect(jsonPath("$.price").value(49.99))
+                .andExpect(jsonPath("$.quantity").value(100));
+    }
+
+    @Test
+    public void shouldGetAllConsoles() throws Exception {
+        when(consoleRepository.findAll()).thenReturn(Arrays.asList(console));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/consoles"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].model").value("PlayStation 5"))
+                .andExpect(jsonPath("$[0].manufacturer").value("Sony"))
+                .andExpect(jsonPath("$[0].memoryAmount").value("825GB SSD"))
+                .andExpect(jsonPath("$[0].processor").value("Custom AMD Zen 2"))
+                .andExpect(jsonPath("$[0].price").value(49.99))
+                .andExpect(jsonPath("$[0].quantity").value(100));
+    }
+
+    @Test
+    public void shouldGetConsoleById() throws Exception {
+        when(consoleRepository.findById(1)).thenReturn(Optional.of(console));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/console/{id}", 1))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.model").value("PlayStation 5"))
+                .andExpect(jsonPath("$.manufacturer").value("Sony"))
+                .andExpect(jsonPath("$.memoryAmount").value("825GB SSD"))
+                .andExpect(jsonPath("$.processor").value("Custom AMD Zen 2"))
+                .andExpect(jsonPath("$.price").value(49.99))
+                .andExpect(jsonPath("$.quantity").value(100));
+    }
+
+    @Test
+    public void shouldUpdateConsole() throws Exception {
+        when(consoleRepository.save(any(Console.class))).thenReturn(console);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders
+                        .put("/cnsole/{id}", console.getConsoleId())
+                        .content(mapper.writeValueAsString(console))
+                        .contentType(MediaType.APPLICATION_JSON)
+
+        ).andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void shouldDeleteConsoleById() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.delete("/console/{id}", 1)
+
+        ).andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void shouldGetConsoleByManufacturer() throws Exception {
+        when(consoleRepository.findByManufacturer("Sony")).thenReturn(Arrays.asList(console));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/console/manufacturer/{manufacturer}", "Sony"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].model").value("PlayStation 5"))
+                .andExpect(jsonPath("$[0].manufacturer").value("Sony"))
+                .andExpect(jsonPath("$[0].memoryAmount").value("825GB SSD"))
+                .andExpect(jsonPath("$[0].processor").value("Custom AMD Zen 2"))
+                .andExpect(jsonPath("$[0].price").value(49.99))
+                .andExpect(jsonPath("$[0].quantity").value(100));
+    }
+
+
+
+
+
+
 
 
 }
