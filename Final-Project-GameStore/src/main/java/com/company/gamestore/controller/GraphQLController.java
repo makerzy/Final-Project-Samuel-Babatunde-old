9abf2de8ef193270ded8b4916707ad750cb6a867
makerzy <botassammzy@gmail.com>
2023-08-31 05:43:55 -0700

    fixed security ish

--- Final-Project-GameStore/src/main/java/com/company/gamestore/controller/GraphQLController.java
+++ Final-Project-GameStore/src/main/java/com/company/gamestore/controller/GraphQLController.java
@@ -7,9 +7,12 @@ import com.company.gamestore.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
+
+@Controller
 public class GraphQLController {
 
     @Autowired
